567158819f35dceb266163b00979c09212fee519
Arévalo Araque <arevalo@atcodes.co>
2020-02-04 13:00:27 -0300

    README.md

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='atcodes-chappie',
+    name='atcodes_chappie',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1',
 
     description='A collection of tools and utilities commonly for python projects.',
     long_description=long_description,
